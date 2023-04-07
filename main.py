ae5000cc857cfa34b1243942065410df410cf979
Murari Prasad <murariprasad2001@gmail.com>
2023-04-07 22:22:13 +0530

    added audio play for similarity

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import files_access
 import db_access
 import similarity
-
+import base64
 
 # Configuring the Main Stage
 st.set_page_config(page_title="Music Selection", layout='wide')
@@ -18,8 +18,10 @@ def audio_names_from_db():
 
 @st.cache_data(show_spinner=False)
 def similarity_calculation():
-    enc_files = pd.DataFrame(db_access.read_db("SELECT encoding_name FROM `audio-data`"))['encoding_name'].values.tolist()
-    encodings = [np.load(files_access.return_np_encoding(f)) for f in enc_files]
+    enc_files = pd.DataFrame(
+        db_access.read_db("SELECT encoding_name FROM `audio-data`")
+    )['encoding_name'].values.tolist()
+    encodings = np.array([np.load(files_access.return_np_encoding(f)) for f in enc_files])
     sim_vector = similarity.ts_ss(encodings)
 
     return sim_vector.detach().cpu().numpy().copy()
@@ -36,7 +38,14 @@ def initializer():
     alert.empty()
 
 
+# initialize cache
 initializer()
+
+# Page elements setup
+st.markdown("""<style>div.block-container{padding-top:0rem;}</style>""", unsafe_allow_html=True)
+# with open("assets/top5.jpg", "rb") as f:
+#     data = base64.b64encode(f.read()).decode("utf-8")
+# st.markdown(f"""<img src="data:image/png;base64,{data}" width="100%" height="150">""", unsafe_allow_html=True)
 st.title("Feature Selection based Music Selection")
 col1, col2 = st.columns([3, 7])  # 2 vertical Sections [30%, 70%]
 
@@ -48,10 +57,11 @@ with col1:
     if option != '-----Select Option-----':
         with st.container():
             st.markdown(f"Your Selection: **{option}**")
-            selection = pd.DataFrame(db_access.read_db(f"SELECT genre, spec_name, audio_name FROM `audio-data` WHERE name = '{option}'"))
+            selection = pd.DataFrame(
+                db_access.read_db(f"SELECT genre, spec_name, audio_name FROM `audio-data` WHERE name = '{option}'"))
 
             spec = files_access.return_spectrogram(selection['spec_name'].values[0])
-            audio_path = selection['genre'].values[0]+"/"+selection['audio_name'].values[0]
+            audio_path = selection['genre'].values[0] + "/" + selection['audio_name'].values[0]
             audio = files_access.return_audio(audio_path)
 
             st.image(spec)
@@ -59,25 +69,37 @@ with col1:
 
         with col2:
 
-            ind = options_list.index(option)  # -1 because of stock option at ind 0
-            top_ind = np.argsort(similarity_calculation()[ind])[1:4]  #
+            ind = options_list.index(option)
+            top_ind = np.argsort(similarity_calculation()[ind])[:5]  #
             top_audio = [options_list[i] for i in top_ind]
+            if option in top_audio:
+                top_audio.remove(option)
             top_sim = [similarity_calculation()[ind][i] for i in top_ind]
 
-            spec_names = pd.DataFrame(
-                db_access.read_db(f"""SELECT spec_name FROM `audio-data` WHERE name IN("{top_audio[0]}", "{top_audio[1]}", "{top_audio[2]}")""")
-            )['spec_name'].values.tolist()
+            sim_query = pd.DataFrame(
+                db_access.read_db(
+                    f"""SELECT spec_name, audio_name, genre FROM `audio-data` WHERE name IN("{top_audio[0]}", "{top_audio[1]}", "{top_audio[2]}")""")
+            )
+            spec_names = sim_query['spec_name'].values.tolist()
+            audio_names = sim_query['audio_name'].values.tolist()
+            genres_names = sim_query['genre'].values.tolist()
             top1 = files_access.return_spectrogram(spec_names[0])
+            top1_audio = files_access.return_audio(genres_names[0] + "/" + audio_names[0])
             top2 = files_access.return_spectrogram(spec_names[1])
+            top2_audio = files_access.return_audio(genres_names[1] + "/" + audio_names[1])
             top3 = files_access.return_spectrogram(spec_names[2])
+            top3_audio = files_access.return_audio(genres_names[2] + "/" + audio_names[2])
 
-            sec1, sec2, sec3 = st.columns([1, 1, 1])  # 2 vertical Sections [30%, 70%]
+            sec1, sec2, sec3 = st.columns([1, 1, 1])
             with sec1:
                 st.write(top_audio[0])
                 st.image(top1)
+                st.audio(top1_audio)
             with sec2:
                 st.write(top_audio[1])
                 st.image(top2)
+                st.audio(top2_audio)
             with sec3:
                 st.write(top_audio[2])
                 st.image(top3)
+                st.audio(top3_audio)
